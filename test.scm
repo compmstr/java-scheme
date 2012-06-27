e657800408738a6ac20284519c726a4ab6085e25
corey williams <corey@undihp.undecided>
2012-06-27 16:06:39 -0400

    Added test script

---
+++ test.scm
@@ -0,0 +1,19 @@
+(define (factorial n)
+  (define (iter product counter max-count)
+    (if (> counter max-count)
+      product
+      (iter (+ counter product)
+            (+ counter 1)
+            max-count)))
+  (iter 1 1 n))
+
+"(factorial 5):"
+(factorial 5)
+
+(define (range-recur cur target)
+  (if (= cur target)
+    (list target)
+    (cons cur (range-recur (+ cur 1) target))))
+
+(define (range x)
+  (range-recur 0 x))
